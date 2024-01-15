b5a71252a00e9408ce8f4990aa95281430911397
None083 <perezgonzaleznoelia08@gmail.com>
2024-01-15 09:56:09 +0100

    Añade constructores en clase modelo

--- src/main/java/daw/Cancion.java
+++ src/main/java/daw/Cancion.java
@@ -13,5 +13,16 @@ public class Cancion {
     private String titulo;
     private String interprete;
     private int duracion;
+
+    public Cancion(String titulo, String interprete, int duracion) {
+        this.titulo = titulo;
+        this.interprete = interprete;
+        this.duracion = duracion;
+    }
+
+    public Cancion() {
+    }
+    
+    
     
 }
